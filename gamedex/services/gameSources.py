d1532ba9377750466834eba6be0e4280a7df59c8
codecollision <michael@codecollision.com>
2013-06-15 17:23:40 -0700

    import bug

--- gamedex/services/gameSources.py
+++ gamedex/services/gameSources.py
@@ -65,22 +65,21 @@ def getSteamGames(id):
 def getPSNGames(id):
 
      # playstation user games/trophy url
-    url = 'http://us.playstation.com/playstation/psn/profile/get_title_progress_data'
+    url = 'http://us.playstation.com/playstation/psn/profile/' + id + '/get_ordered_trophies_data'
 
     # headers required for response
-    headers = {'Host': 'us.playstation.com', 'X-Requested-With': 'XMLHttpRequest', 'User-Agent': 'Mozilla', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept': 'text/html', 'Accept-Encoding': 'gzip'}
-
-    # construct form data
-    form_fields = {
-        'sortBy': '',
-        'ids': id
+    headers = {
+        'Host': 'us.playstation.com',
+        'X-Requested-With': 'XMLHttpRequest',
+        'User-Agent': 'Mozilla',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept': 'text/html',
+        'Accept-Encoding': 'gzip',
+        'Cookie': 'SONYCOOKIE1=677685440.20480.0000; __unam=756c574-13f21832b06-3a7fa02-1; s_cc=true; s_sq=%5B%5BB%5D%5D; s_vi=[CS]v1|28D68036851D1C3F-6000012C40010A16[CE]; TICKET=MQAAAAAAAPgwAACsAAgAFADR5abPxLs4rBysKuLG8KfkyEcbAAEABAAAAQAABwAIAAABP0oPi1AA%0ABwAIAAABP0815uAAAgAIfd4skexVS6sABAAgTWljaHUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%0AAAAACAAEdXMAAQAEAARhNgAAAAgAGFVQOTAwMi1OUFdBMDAwMzVfMDAAAAAAADARAAQHvwEcAAEA%0ABB4AAgAwEAAAAAAAADACAEQACAAE2%2B8LsgAIADgwNgIZAJVhqTJ9ZKQ2ornliP9bM6Q3s7AvBL1o%0AxwIZAI7HkjNkzHGNFFGrroEXi1qaHTJf5FTR%2FA%3D%3D; PSNS2STICKET=MQAAAAAAAPgwAACsAAgAFDDDhl72dxY1vp7Q5cvzD1LSy8JlAAEABAAAAQAABwAIAAABP0oPi7UA%0ABwAIAAABP0815uAAAgAIfd4skexVS6sABAAgTWljaHUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%0AAAAACAAEdXMAAQAEAARhNgAAAAgAGElWMDAwMS1OUFhTMDEwMDRfMDAAAAAAADARAAQHvwEcAAEA%0ABB4AAgAwEAAAAAAAADACAEQACAAEyS7rGwAIADgwNgIZAK1aVaE866zmIoUlf3U3rIxlD8pq5g08%0ApwIZAKrRjrnMhwDSW%2FGzMoaEHP9cez1SQb%2Bh4Q%3D%3D;'
     }
 
-    # serialize form data
-    form_data = urllib.urlencode(form_fields)
-
     # fetch url - send form data, allow 30 seconds for response
-    response = urlfetch.fetch(url=url, payload=form_data, method=urlfetch.POST, headers=headers, deadline=30)
+    response = urlfetch.fetch(url=url, method=urlfetch.GET, headers=headers, deadline=30)
 
     # valid response - begin parse
     if response.status_code == 200:
@@ -96,8 +95,8 @@ def getPSNGames(id):
         # iterate game list
         html = etree.HTML(content)
 
-        titleSectionSel = CSSSelector('.contentbox_compare')
-        titleSel = CSSSelector('.gametitle span')
+        titleSectionSel = CSSSelector('.recentitems')
+        titleSel = CSSSelector('.gametitle a')
 
         # for each game > get title
         for row in titleSectionSel(html):
